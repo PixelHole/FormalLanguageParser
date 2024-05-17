6494ee226ba5db23cf24bdab16e2fb5eee57ce28
PixelHole <66640043+PixelHole@users.noreply.github.com>
2024-05-17 22:40:01 +0330

    push to github

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace GrammarUnderstander;
+
+static class Program
+{
+    private static StandardGrammar grammar;
+    private static string[] Inputs;
+
+    public static void Main(string[] args)
+    {
+        CreateGrammar();
+        GetInputs();
+        CheckInputs();
+    }
+
+    private static void CreateGrammar()
+    {
+        try
+        {
+            grammar = new StandardGrammar(GetRules());
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+            CreateGrammar();
+        }
+    }
+    private static List<string> GetRules()
+    {
+        List<string> rules = new List<string>();
+
+        Console.WriteLine("Enter Rule Count : ");
+        int ruleCount = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter rules in the following format -> RuleLabel(Char)->Rule(String)");
+        for (int i = 0; i < ruleCount; i++)
+        {
+            rules.Add(Console.ReadLine());
+        }
+
+        return rules;
+    }
+
+    private static void GetInputs()
+    {
+        int inputCount = int.Parse(Console.ReadLine());
+
+        Inputs = new string[inputCount];
+        
+        for (int i = 0; i < inputCount; i++)
+        {
+            Inputs[i] = Console.ReadLine();
+        }
+    }
+
+    private static void CheckInputs()
+    {
+        foreach (string input in Inputs)
+        {
+            Console.WriteLine(grammar.isStringValid(input) ? "Yes" : "No");
+        }
+    }
+}
---
+++ StandardGrammar.cs
@@ -0,0 +1,118 @@
+﻿namespace GrammarUnderstander;
+
+public class StandardGrammar
+{
+    private List<GrammarRule> Rules = new List<GrammarRule>();
+    
+    public StandardGrammar(List<string> rawRules)
+    {
+        Rules = ParseRules(rawRules);
+    }
+    private List<GrammarRule> ParseRules(List<string> rawRules)
+    {
+        List<GrammarRule> rules = new List<GrammarRule>();
+
+        foreach (var rawRule in rawRules)
+        {
+            string[] sections = rawRule.Split("->");
+            if (sections.Length != 2) throw new ArgumentException("please enter the rule in the correct format");
+            rules.Add(new GrammarRule(sections[0][0], sections[1]));
+        }
+
+        return rules;
+    }
+    
+    // you wanna look at here ↓
+    public bool isStringValid(string input)
+    {
+        // List of all permutations
+        List<string> permutations = new List<string>();
+
+        /*
+         add all main rules (rules that usually start with S. in code, rules that have the same label as the first rule)
+         to the permutations list
+        */
+        foreach (var mainRule in Rules.FindAll(rule => rule.Label == Rules[0].Label))
+        {
+            permutations.Add(mainRule.Body);
+        }
+
+        // for each character of the input check all permutations at that index
+        for (int i = 0; i < input.Length; i++)
+        {
+            int checkIndex = 0;
+            
+            // while loop to check all permutations
+            while (checkIndex < permutations.Count)
+            {
+                // save a copy of the current permutation that we are checking
+                string permutation = permutations[checkIndex];
+
+                // if the permutation at that index is the same as the input, move on to the next
+                if (permutation[i] == input[i])
+                {
+                    checkIndex++;
+                    continue;
+                }
+
+                /*
+                 * from here on, all logic is based on the fact that the permutation character at that index does not
+                 * match the input character at said index
+                 */
+                
+                // check if there is a rule with the same label as the character in the permutation
+                if (Rules.Exists(rule => rule.Label == permutation[i]))
+                {
+                    // if found grab all the rules that have that label
+                    var foundRules = Rules.FindAll(rule => rule.Label == permutation[i]);
+                    
+                    // remove that permutation from the permutations list to avoid duplicates 
+                    permutations.Remove(permutation);
+                    // remove the rule label from the permutation so we can insert new permutations
+                    permutation = permutation.Remove(i, 1);
+
+                    // insert the body of each rule at the index that we removed the label
+                    foreach (var rule in foundRules)
+                    {
+                        string newMutation = permutation.Insert(i, rule.Body);
+                        
+                        /*
+                         if the generated string is longer than the input, dont insert it
+                         this is to avoid infinite loops and also save time
+                         */
+                        if (newMutation.Length <= input.Length)
+                        {
+                            checkIndex = 0;
+                            permutations.Add(newMutation);
+                        }
+                    }
+                    
+                    // if we have generated new mutations, start the checking process from scratch
+                    // this can definitely be improved but i cannot be fucked
+                    if(checkIndex == 0) continue;
+                }
+                
+                // if the character does not match and it is not a rule, remove the mutation and move on
+                permutations.Remove(permutation);
+            }
+
+            // if no mutations are in currently in the list this means that the input string does not follow the grammar
+            // therefore you can safely return false
+            if (permutations.Count == 0) return false;
+        }
+
+        return true;
+    }
+}
+
+internal class GrammarRule
+{
+    public char Label { get; }
+    public string Body { get; }
+
+    public GrammarRule(char label, string body)
+    {
+        Label = label;
+        Body = body;
+    }
+}
